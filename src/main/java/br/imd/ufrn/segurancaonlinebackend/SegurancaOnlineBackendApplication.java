06ddf8f62cdaf74203395a68c5b9ff0b38f0eb6a
martinsspn <47855595+martinsspn@users.noreply.github.com>
2023-04-14 18:40:32 -0300

    atualização gitignore

--- src/main/java/br/imd/ufrn/segurancaonlinebackend/SegurancaOnlineBackendApplication.java
+++ src/main/java/br/imd/ufrn/segurancaonlinebackend/SegurancaOnlineBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "br.imd.ufrn.segurancaonlinebackend", "br.imd.ufrn.segurancaonlinebackend.config" })
 public class SegurancaOnlineBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(SegurancaOnlineBackendApplication.class, args);
